204ae6673a35c380b7d1d51a049c328aee18cb4d
chai <chaitanyagupta759@gmail.com>
2024-05-04 19:10:48 +0530

    cors URL added

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://real-time-chat-app-frontend-c2al.onrender.com",
     credentials: true,
   },
 });
